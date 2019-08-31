9b9452e0eca2f8413a0c4fbd4ff9f7aaa7994d98
emerson2pe <sala302b@ceines.com.br>
2019-08-30 21:37:38 -0300

    AtualizadoDia30

--- ConversoTamanho/app/src/main/java/br/senac/es/conversotamanho/MainActivity.java
+++ ConversoTamanho/app/src/main/java/br/senac/es/conversotamanho/MainActivity.java
@@ -13,23 +13,37 @@ import java.util.Locale;
 public class MainActivity extends AppCompatActivity {
 
     int alturaEmCentimentros = 0;
+    int altura1 = 100;
+    int altura2 = 150;
+    int altura3 = 210;
+
+    public int mediaValores(int valor1, int valor2, int valor3) {
+
+        return (valor1 + valor2 + valor3) / 3;
+    }
 
 
     @Override
     //Protected
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //chama todo codigo da classe pai.
 
         //metodo que liga a MainActivity.JAVA no activity_main.xml (TELA)
         setContentView(R.layout.activity_main);
+
 
         //Instacia de um elemento texto do android
         final TextView txtMetros = (TextView) findViewById(R.id.txtMetros);
         //Exemplo Semelhante de instacia objeto no java
         //final textview textMetros = new TextView();
-
         //txtMetros.setText(" "+ alturaEmCentimentros);
         //Cria um string vazia concatenada.
+        final int media = mediaValores(altura1, altura2, altura3);
+
+        final TextView txtMediaAltura = (TextView) findViewById(R.id.txtMediaAltura);
+
+        final TextView txtMedia = (TextView) findViewById(R.id.txtMedia);
 
         final TextView txtPes = (TextView) findViewById(R.id.txtPes);
 
@@ -44,6 +58,7 @@ public class MainActivity extends AppCompatActivity {
         seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             // monitora e atualiza as 3 funções.
 
+
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean b) {
                 //quando usuario move no SeekBar e para ele pega o valor e guarda.
@@ -55,8 +70,11 @@ public class MainActivity extends AppCompatActivity {
                 txtMetros.setText(texto);
                 //informa o valor do texto.
 
+
+
             }
 
+            //subcreve
             @Override
             public void onStartTrackingTouch(SeekBar seekBar) {
                 //quando usuario para de mover o SeekBar ele da a menssagem que precisa converter.
@@ -68,32 +86,53 @@ public class MainActivity extends AppCompatActivity {
                 //quando usuario para de mover no SeekBar.
             }
 
+
         });
         final Button btnConverter = (Button) findViewById(R.id.btnConverter);
         btnConverter.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
+                //variavel double AlturaEmPes
                 double alturaEmPes = alturaEmCentimentros / 30.48;
                 //alturaEmPes recebe o valor alturaEmCentimentros / 30.48.
                 String texto = formataValorComDoisDigitos(alturaEmPes);
                 //converte para String e deixa o valor com 2 casas.
                 texto += "pé(s)";
+                //concatenando texto com pes
                 txtPes.setText(texto);
+
+
+                double mediaAltura = media ;
+                String texto1 = formataValorComDoisDigitos(mediaAltura / 100.0);
+                texto1 += "m.";
+                txtMediaAltura.setText(texto1);
+
+                if (alturaEmCentimentros > media) {
+                    txtMedia.setText("Alto");
+
+                } else {
+                    txtMedia.setText("Baixo");
+                }
             }
+
         });
 
     }
 
+    //entidade String metodo e argumento
     private String formataValorComDoisDigitos(double valor) {
         return String.format(Locale.FRANCE, "%.2f", valor);
         //retorna um local, pega valor e formata.
-
+        //format " %.2f" como valor sai
 
     }
 
 
 }
+
+
+
 
 
 
